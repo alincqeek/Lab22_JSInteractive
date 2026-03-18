55aaf7262bd43fc1868ac78be3b2572ba6b75389
alincqeek <a72658695@gmail.com>
2026-03-18 11:38:00 +0300

    5.15

--- calculator/main.js
+++ calculator/main.js
@@ -1,29 +1,29 @@
-const display=document.getElementById("display");
-const buttons=document.getElementById("buttons");
-const clearBtn=document.getElementById("clear");
-let expression="";
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear");
+let expression = "";
 buttons.forEach((button) => {
-    button.addEventListener("click", ()=>{
-       const value=button.textContent; 
-       if (value==="C"){
-        expression="";
-        display.value="";
-        return;
-       }
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+    if (value === "C") {
+      expression = "";
+      display.value = "";
+      return;
+    }
 
-        if (value==="="){
-            try{
-               expression=eval(expression);
-                display.value=expression;
-
-            }catch{
-                display.value="Ошибка";
-                expression="";
-            }
-            return;
-       }
-       expression+=value;
-       display.value=expression;
-    })
-    
+    if (value === "=") {
+      try {
+        expression = eval(expression);
+        display.value = expression;
+      } catch {
+        display.value = "Ошибка";
+        expression = "";
+      }
+      return;
+    }
+    expression += value;
+    display.value = expression;
+  });
 });
+
+import { Calculator } from "./calculator";
